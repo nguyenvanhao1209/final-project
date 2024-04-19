412609bf73d0ccd952649a6aeec99f3b567dfe65
nguyenhoan1209 <nguyenvanhoan0902@gmail.com>
2024-04-19 20:31:40 +0700

    fix: fix self target kho vkl

--- google_login.py
+++ google_login.py
@@ -81,7 +81,7 @@ def google_login():
             <div style="position: absolute; margin-top: 1px; margin-left: 1px; width: 40px; height: 40px; border-radius: 2px; background-color: #fff;">
                 <img style="position: absolute; margin-top: 11px; margin-left: 11px; width: 18px; height: 18px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" />
             </div>
-            <p style="position: absolute; margin: 11px 11px 0 110px; color: #fff; font-size: 14px; letter-spacing: 0.2px; font-family: 'Roboto';">
+            <p style="position: absolute; margin: 11px 11px 0 115px; color: #fff; font-size: 14px; letter-spacing: 0.2px; font-family: 'Roboto';">
             <b> Sign in with Google</b>
             </p>
         </div>
